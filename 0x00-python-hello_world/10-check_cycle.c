79254ca06aec77c65c0a0257ce99ddfafb401f83
Neivanny1 <nevileneivanny@gmail.com>
2023-08-07 04:41:58 -0700

    Betty errors fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,24 +5,21 @@
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
 
- int check_cycle(listint_t *list) {
-    listint_t *slow, *fast;
-
-    if (!list)
-        return 0;
-
-    slow = list;
-    fast = list;
-
-    while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
-            return (1);
-    }
-
-    return (0);
+	if (!list)
+		return (0);
+	slow = list;
+	fast = list;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
 }
 
