137d97b1139b9e2da25e42d3bdab18fa03d1c4f9
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-08-07 11:30:46 +0000

    Check if a singly linked list has a cycle

--- 0x00-python-hello_world/check_cycle.c
+++ 0x00-python-hello_world/check_cycle.c
@@ -1,5 +1,28 @@
 #include "lists.h"
+/**
+ * check_cycle - Check if a singly linked list has a cycle
+ * @list: Pointer to the head of the linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 
-int check_cycle(listint_t *list)
-{
+ int check_cycle(listint_t *list) {
+    listint_t *slow, *fast;
+
+    if (!list)
+        return 0;
+
+    slow = list;
+    fast = list;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return (1);
+    }
+
+    return (0);
+}
 
