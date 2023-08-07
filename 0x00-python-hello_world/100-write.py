86e012b366ae22b05a3aaab15b773b3c44ab1370
Neivanny1 <nevileneivanny@gmail.com>
2023-08-07 16:18:10 +0300

    printing using write from the sys module

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-dora = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(dora +  "\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
