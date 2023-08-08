e71ad3f1e72ea8d1c5f29b8792573a5cc4a2949c
Neivanny1 <nevileneivanny@gmail.com>
2023-08-08 16:37:47 +0300

    More conditional statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-is_last = str(number)
-last_str = is_last[-1]
-last = int(last_str)
+last = int(math.fmod(number, 10))
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last < 6 and last != 0:
