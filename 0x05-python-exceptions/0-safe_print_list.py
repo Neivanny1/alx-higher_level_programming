72f7173a0b9467784f97fb977b21d36fdae8ff4e
Neivanny1 <nevileneivanny@gmail.com>
2023-09-02 16:25:56 +0300

    Pycodestyle error fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/pyton3
 def safe_print_list(my_list=[], x=0):
     num = 0
-    for x in range (0, x):
+    for x in range(0, x):
         try:
             print(my_list[x], end="")
             num += 1
