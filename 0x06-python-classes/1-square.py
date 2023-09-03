b37c8a930073f365f21581249cd77b580236514f
Neivanny1 <nevileneivanny@gmail.com>
2023-09-03 13:38:18 +0300

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Square with a size attribute
+"""
+
+
+class Square:
+    """ A square class with private size attribute """
+    def __init__(self, size):
+        self.__size = size
