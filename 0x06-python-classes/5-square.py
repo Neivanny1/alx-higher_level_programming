7bfc78670f76d6fd2f6aa224fc318b48a3667712
Neivanny1 <nevileneivanny@gmail.com>
2023-09-03 14:35:03 +0300

    Checks for coordiante

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,9 +6,8 @@ Defines class Square
 
 class Square:
     """ defines private attribute """
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         self.__size = size
-        self.__position = position
 
     @property
     def size(self):
@@ -23,35 +22,13 @@ class Square:
         else:
             self.__size = value
 
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if all(x is not int for x in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if all(x < 0 for x in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = position
-
     """ prints square with # in stdout """
     def my_print(self):
         if self.size == 0:
             print()
         else:
-            x = self.position[0]
-            y = self.position[1]
-            for i in range(y):
-                print("")
-            for row in range(self.size):
-                for j in range(x):
-                    print(" ", end="")
-                for col in range(self.size):
+            for i in range(self.size):
+                for j in range(self.size):
                     print("#", end="")
                 print()
 
