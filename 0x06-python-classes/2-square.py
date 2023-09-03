08b58eba122789e80a860c804e767d9aa5ae573c
Neivanny1 <nevileneivanny@gmail.com>
2023-09-03 14:20:26 +0300

    Too much effect

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ Defines a class Square with a size attribute
 class Square:
     """ A square class with optional  private size attribute """
     def __init__(self, size=0):
+        self.__size = size
         if size < 0:
             raise ValueError("size must be >= 0")
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        self.__size = size
