0011e5df57d5c0abf055137e76bb0bc2859ff4ee
Neivanny1 <nevileneivanny@gmail.com>
2023-09-04 23:09:09 +0300

    Tracking number of instances created

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ Defines class Rectangle
 class Rectangle:
     """ Rectangle class intialisation """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
