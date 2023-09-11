98d22c7c7cfd0a9e67bfeaf7b9ad13a8df02ba37
Neivanny1 <nevileneivanny@gmail.com>
2023-09-11 12:53:29 -0700

    Sytle issue fixed

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,7 +5,6 @@ Imports class BaseGeometry
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-
 class Rectangle(BaseGeometry):
     """ initializes class """
     def __init__(self, width, height):
@@ -13,11 +12,9 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
     """ public instance method area """
     def area(self):
         return self.__width * self.__height
-
     """ prints rectangle description """
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
