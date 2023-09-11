4f8176649b08833c4d552273f7dc2a2a9be277e2
Neivanny1 <nevileneivanny@gmail.com>
2023-09-11 12:38:39 -0700

    raisng exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines an empty class
+"""
+
+
+class BaseGeometry:
+
+    """Raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
