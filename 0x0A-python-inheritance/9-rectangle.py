1dea450625a1796b669c2c778d3f4026ff411508
Neivanny1 <nevileneivanny@gmail.com>
2023-09-11 12:50:46 -0700

    X-tics inheritance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Imports class BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+
+class Rectangle(BaseGeometry):
+    """ initializes class """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    """ public instance method area """
+    def area(self):
+        return self.__width * self.__height
+
+    """ prints rectangle description """
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
