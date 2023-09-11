0a1a38f8aea4c7354852ed4950db18640e9f77b0
Neivanny1 <nevileneivanny@gmail.com>
2023-09-11 12:23:10 -0700

    the object is an instance of a class that inherited (directly or indirectly) from the specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to check if the object is an instance of a class that inherited (directly or indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    return true if object is instance of class else false
+    """
+    if isinstance(obj, a_class) and not issubclass(a_class, type(obj)):
+        return True
+    else:
+        return False
