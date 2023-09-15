4cbb40913b4d387630b15ed331d7eaf77ab501e1
Neivanny1 <nevileneivanny@gmail.com>
2023-09-15 09:27:31 -0400

    adding validation in all getters and setters methods and instantiation in class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Defines a class called Rectangle
+Module to import class Base
 """
 from models.base import Base
 
@@ -15,25 +15,50 @@ class Rectangle(Base):
         super().__init__(id)
     @property
     def width(self):
+        """gets width"""
         return self.__width
     @width.setter
     def width(self, value):
+        """sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <=0:
+            raise ValueError("width must be > 0")
         self.__width = value
     @property
     def height(self):
+        """gets height"""
+
         return self.__height
     @height.setter
     def height(self, value):
+        """sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
     @property
     def x(self):
+        """gets x"""
         return self.__x
     @x.setter
     def x(self, value):
+        """sets x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
     @property
     def y(self):
+        """gets y"""
         return self.__y
     @y.setter
     def y(self, value):
+        """sets y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
