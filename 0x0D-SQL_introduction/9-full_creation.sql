39d004d23b8381d68e38c751b323dd4d7a977f83
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-10-18 00:45:25 +0300

    a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
--- displays the number of records with 'id = 89' in the table 'first_table'
-SELECT COUNT(*) FROM first_table WHERE id = '89';
+-- creates a table 'second_table' in database 'hbtn_0c_0' and adds
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (4, 'George', 8);
