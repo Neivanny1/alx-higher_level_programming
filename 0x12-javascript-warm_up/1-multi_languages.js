ea6dc89a3746e4d269fde1aebe7a005542fad1ab
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-11-13 12:53:26 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const c = "C is fun";
-const p = "Python is cool";
-const j = "JavaScript is amazing";
-console.log(c);
-console.log(p);
-console.log(j);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
