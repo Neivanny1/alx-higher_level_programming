7d72ac588af7de278a1c09d318802edf1023d48d
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-11-14 15:36:58 +0300

    script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { dict } = require('./101-data.js');
+
+const nDict = {};
+for (const key in dict) {
+  if (nDict[dict[key]] === undefined) {
+    nDict[dict[key]] = [];
+  }
+  nDict[dict[key]].push(key);
+}
+console.log(nDict);
