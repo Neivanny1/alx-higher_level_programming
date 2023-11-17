9c0c53eacd6c6a0df65b031bdcb1a245398f12bd
root <nevileneivanny@gmail.com>
2023-11-17 07:58:37 -0800

    filtering inputs to crb injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module script to list state passed as arg
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name=%s
+                 ORDER BY states.id ASC;""".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
