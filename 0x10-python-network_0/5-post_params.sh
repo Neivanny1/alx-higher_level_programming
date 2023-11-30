52ce2fb4e859a68f2f9f2bd028259aa615b7b0a7
Neivanny1 <nevileneivanny@gmail.com>
2023-11-30 12:11:20 -0800

    sending emails

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -s "$1" -X POST -d "email=test@gmail.com.com&subject=I%20will%20always%20be%20here%20for%20PLD"
+#takes in a URL, sends a POST request to the passed URL,and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
