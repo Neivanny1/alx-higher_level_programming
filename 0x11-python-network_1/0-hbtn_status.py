cf51644525ae40acbb2f6aed65bdb0e98aa6e6c1
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-12-01 23:53:04 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,14 @@ import urllib.request
 import urllib.parse
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+
+def fetch_url():
+        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type:', type(html))
         print('\t- content:', html)
         print('\t- utf8 content:', html.decode('UTF-8'))
+
+if __name__ == "__main__":
+    fetch_url()
