9fd6bdd4cbd9f1aba006bd9af7a1f5438d91e92c
Neivanny1 <nevileneivanny@gmail.com>
2023-12-01 12:32:50 -0800

    fetching urls with python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ Module that fetches a url
 
 import urllib.request
 
-def fetch_url():
+
+if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
@@ -13,7 +14,3 @@ def fetch_url():
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode()))
-
-
-if __name__ == "__main__":
-    fetch_url()
