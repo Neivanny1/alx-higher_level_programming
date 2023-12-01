4fc9e546b1fa00d478d4b1f241529a5813aca062
Neivanny1 <nevileneivanny@gmail.com>
2023-12-01 12:57:01 -0800

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""
+Module that fetches https://alx-intranet.hbtn.io/status
+"""
 
 import urllib.request
 import urllib.parse
