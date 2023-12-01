8d4c0b825eda177258669caf7ff637f5df83198b
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-12-01 23:55:33 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status"""
+
 import urllib.request
 import urllib.parse
 
 
-
-def fetch_url():
-        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         print('Body response:')
         print('\t- type:', type(html))
         print('\t- content:', html)
         print('\t- utf8 content:', html.decode('UTF-8'))
-
-if __name__ == "__main__":
-    fetch_url()
