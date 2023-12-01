97fc32e4968d40abafe6a460458240a2a452072c
Neivanny1 <nevileneivanny@gmail.com>
2023-12-01 13:49:35 -0800

    sending request to url

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to send request to url
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    try:
+        print(r.headers['X-Request-Id'])
+    except:
+        pass
