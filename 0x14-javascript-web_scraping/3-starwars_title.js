ca3f8f97e2c3ec8780302760c7f375b04a0fabc6
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2024-01-03 15:39:17 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
 // prints the title of a Star Wars movie where the episode number
-// matches a given integer
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error('Unexpected response status code:', response.statusCode);
-  } else {
-    console.log('Response body:', body); // Log the entire response body for inspection
-    try {
-      const data = JSON.parse(body);
-      console.log('Title:', data['title']);
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-    }
-  }
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
 });
