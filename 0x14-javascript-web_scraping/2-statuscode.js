2e2f06d46fb833133c91b4e934c3eee253bee564
root <nevileneivanny@gmail.com>
2024-01-03 04:21:54 -0800

    standards applied

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log('code: ' + response['statusCode']);
+    console.log('code: ' + response.statusCode);
   }
 });
