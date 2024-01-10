4957ca5ae3c53b68930ce2576a6aa016a5afb469
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2024-01-10 15:26:54 +0000

    adds color when user clicks

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// updates the text color of HTML tag HEADER to red when the user
+// clicks on the tag DIV#red_header
+$('#red_header').click(function () {
+    $('#red_header').css('color', '#FF0000');
+  });
